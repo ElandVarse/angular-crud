3220ed49bb3c01c51264141ab23c6f46ce6a4fb2
Andre <andre.jobs23@gmail.com>
2021-05-01 10:30:16 -0300

    Post products

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,35 @@
+import { Product } from './../product.model';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { Router } from '@angular/router';
+
+@Component({
+	selector: "app-product-create",
+	templateUrl: "./product-create.component.html",
+	styleUrls: ["./product-create.component.css"],
+})
+export class ProductCreateComponent implements OnInit {
+
+    product: Product = {
+        name: '',
+        price: null,
+    }
+    
+    constructor(private productService: ProductService, private router:Router) {}
+
+	ngOnInit(): void {
+    }
+
+    createProduct(): void{
+        this.productService.create(this.product).subscribe(() => {
+            this.productService.showMessage('Produto criado!')
+            this.router.navigate(['/products'])
+        })
+
+        this.productService.showMessage('Produto criado')
+    }
+ 
+    cancel(): void{
+        this.router.navigate(['/products'])
+    }
+}
